609ea133876c4aa91c8dcb5178925470a34fddc4
markusm88 <markusmorland@gmail.com>
2015-09-22 23:27:31 +0200

    fixed navbar

--- hovedprosjekt-web/js/hp-costum.js
+++ hovedprosjekt-web/js/hp-costum.js
@@ -62,11 +62,11 @@ $(function () {
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
                 $('html,body').animate({
-                    scrollTop: target.offset().top
+                    scrollTop: target.offset().top - 80
                 }, 1000);
                 return false;
             }
         }
     });
-    
+
 });
